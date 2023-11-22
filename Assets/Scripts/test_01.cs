44e476605b6f2cf785de5a699da0745175297d0e
sancho6040 <qwetito@gmail.com>
2023-11-21 21:32:50 -0500

    test

--- Assets/Scripts/test_01.cs
+++ Assets/Scripts/test_01.cs
@@ -4,15 +4,4 @@ using UnityEngine;
 
 public class test_01 : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
